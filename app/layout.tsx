bbb8acbbf564f98ec0f7fd67e4e63bfac92464d6
fredegd <124848643+fredegd@users.noreply.github.com>
2025-07-03 19:11:39 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'shorty-url-shortener',
   description: 'Yet Another Url Shortener',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
